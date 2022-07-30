689637825b7ec6e58e4ae730775c2e6420dce5c1
tein408 <tein408@gmail.com>
2022-07-30 10:15:51 +0900

    feat: 상수 object 추가

---
+++ src/main/kotlin/Const.kt
@@ -0,0 +1,19 @@
+const val MAX_NAME_LENGTH = 5
+const val RANDOM_RANGE = 10
+const val RACING_CAR = "-"
+
+object Const {
+    object InputMessage {
+        const val CAR_INPUT_MESSAGE = "자동차는 몇 대 인가요?"
+        const val CAR_TRY_MESSAGE = "몇 번 시도 하나요?"
+    }
+
+    object OutputMessage {
+
+    }
+
+    object ErrorMessage {
+
+    }
+
+}
